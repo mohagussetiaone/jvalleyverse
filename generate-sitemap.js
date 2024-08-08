5541cb7a84062fc7b4c07f413d4c71833ee659cc
Moh Agus Setiawan <mohagussetiaone@gmail.com>
2024-08-08 09:36:11 +0700

    add: sitemap.xml

---
+++ generate-sitemap.js
@@ -0,0 +1,24 @@
+import { SitemapStream, streamToPromise } from "sitemap";
+import { createWriteStream } from "fs";
+
+const links = [
+  { url: "/", changefreq: "daily", priority: 1.0 },
+  { url: "/jalur-belajar", changefreq: "weekly", priority: 0.8 },
+  { url: "/tentang", changefreq: "monthly", priority: 0.8 },
+  { url: "/show-case", changefreq: "weekly", priority: 0.8 },
+  { url: "/show-case/buat-show-case", changefreq: "monthly", priority: 0.7 },
+  { url: "/profile", changefreq: "monthly", priority: 0.7 },
+  { url: "/settings", changefreq: "monthly", priority: 0.7 },
+  { url: "/faq", changefreq: "monthly", priority: 0.7 },
+  { url: "/signin", changefreq: "monthly", priority: 0.7 },
+  { url: "/signup", changefreq: "monthly", priority: 0.7 },
+];
+
+const sitemap = new SitemapStream({ hostname: "https://jvalleyverse.vercel.app/" });
+
+streamToPromise(sitemap)
+  .then((data) => createWriteStream("./public/sitemap.xml").write(data))
+  .catch((err) => console.error(err));
+
+links.forEach((link) => sitemap.write(link));
+sitemap.end();
